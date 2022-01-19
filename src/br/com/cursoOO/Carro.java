7b78dd277b1385e8728ba862759dc1ead1b1f4b9
guilherme737-b <guilhermefernandes49@gmail.com>
2022-01-18 23:43:01 -0300

    codigo organizado

--- src/br/com/cursoOO/Carro.java
+++ src/br/com/cursoOO/Carro.java
@@ -3,17 +3,15 @@ package br.com.cursoOO;
 public class Carro {
 
 
-
-
     String medelo;
     int velocidadeMaxima;
     double segundosZeroACem;
 
-    public Carro(){
+    public Carro() {
 
     }
 
-    public Carro(String modelo, int velocidadeMaxima, double segundosZeroACem){
+    public Carro(String modelo, int velocidadeMaxima, double segundosZeroACem) {
         this.medelo = modelo;
         this.velocidadeMaxima = velocidadeMaxima;
         this.segundosZeroACem = segundosZeroACem;
